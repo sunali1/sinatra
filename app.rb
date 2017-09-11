30c5ca7b02724472d15375b22a4f64d49cb57ca5
sunaliseth <sunaliseth01@gmail.com>
2017-09-11 15:38:05 +0100

    Cat with borders

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ get'/try' do
   "test"
 end
 
-get'/cat'do
-"<div>
-   <img src ='http://bit.ly/1eze8aE>'
-</div>"
-end
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+ end
